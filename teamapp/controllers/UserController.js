ba0ba67caa1cea0f31d040ba4e7326ef9ae572a9
jonatavsantos <surinegann@gmail.com>
2023-11-26 22:30:15 -0300

    Check: login compare password

--- teamapp/controllers/UserController.js
+++ teamapp/controllers/UserController.js
@@ -48,7 +48,7 @@ async function SignIn (req, res, next) {
         const token = jwt.sign({ codUser }, process.env.JWT_SECRET, {
             expiresIn: 3600,
         });
-        res.json({ auth: true, token });
+        console.log({ auth: true, token });
     } else {
         throw new Error('Token not found')
     }
--- teamapp/public/js/lib/auth.js
+++ teamapp/public/js/lib/auth.js
@@ -7,30 +7,19 @@ function isAuthenticated() {
 }
 
 function getToken() {
-    try {
-        return localStorage.getItem('TeamApp:token');
-    } catch (error) {
-        console.error('Erro ao obter token do localStorage:', error);
-        return null;
-    }
+    return localStorage.getItem('TeamApp: token');
 }
 
 function login(token) {
-    try {
-        localStorage.setItem('TeamApp:token', token);
-        window.location.href = '/start';
-    } catch (error) {
-        console.error('Erro ao definir token no localStorage:', error);
-    }
+    localStorage.setItem('TeamApp: token', token);
+
+    window.location.href = '#';
 }
 
 function logout() {
-    try {
-        localStorage.removeItem('TeamApp:token');
-        window.location.href = '/login';
-    } catch (error) {
-        console.error('Erro ao remover token do localStorage:', error);
-    }
+    localStorage.removeItem('TeamApp: token');
+
+    window.location.href = '/';
 }
 
 export default { isAuthenticated, getToken, login, logout }
